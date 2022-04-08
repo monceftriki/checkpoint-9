5a7e9632ccb5014be227d23cee65c3aa8af1ee65
moncefTRIKI <monceftriki35@gmail.com>
2022-04-08 11:57:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import First from './components/First';
+import React from 'react'
+import Seconde from './components/Seconde';
+const App = () => {
+  return (
+    <div  className ="App">
+      <Seconde/>
+      <First/>
+    </div>)
+
+ 
+}
+
+export default App
+
---
+++ src/components/First.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+
+
+const First = () => {
+  return ( 
+    <div className="App">
+     <Button variant="primary">Primary</Button>
+    </div>
+  )
+}
+
+export default First
